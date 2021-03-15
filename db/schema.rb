93abb9ed6e86f2848f31f84b53476dc0a9edaac3
angela stephens <angelastephens317@gmail.com>
2021-03-14 20:35:41 -0400

    migrated

---
+++ db/schema.rb
@@ -0,0 +1,44 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_03_12_020728) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "ice_cream_flavors", force: :cascade do |t|
+    t.string "name"
+    t.bigint "sundae_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["sundae_id"], name: "index_ice_cream_flavors_on_sundae_id"
+  end
+
+  create_table "sundaes", force: :cascade do |t|
+    t.string "name"
+    t.integer "scoops"
+    t.integer "like_count"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "toppings", force: :cascade do |t|
+    t.string "name"
+    t.bigint "sundae_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["sundae_id"], name: "index_toppings_on_sundae_id"
+  end
+
+  add_foreign_key "ice_cream_flavors", "sundaes"
+  add_foreign_key "toppings", "sundaes"
+end
