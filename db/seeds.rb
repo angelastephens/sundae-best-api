4836d35142fa3117a43a5bca87d1899eda40f46d
angela stephens <angelastephens317@gmail.com>
2021-03-26 20:34:35 -0400

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-chocolate_bomb = Sundae.find_or_create_by(name:"chocolate_bomb", scoops: 3)
-sundae_best =  Sundae.find_or_create_by(name:"sundae_best", scoops: 2)
+chocolate_bomb = Sundae.find_or_create_by(name:"chocolate_bomb", scoops: 3, ice_cream_flavors: "chocolate, strawberry, dark chocolate", toppings: "chocolate chips, cherries, whipped cream, whoppers, chocolate sauce")
+sundae_best =  Sundae.find_or_create_by(name:"sundae_best", scoops: 2, ice_cream_flavors: "bourbon pecan, vanilla bean", toppings:"whipped cream, carmel sauce")
+
