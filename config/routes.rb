924e424f969f73c44ee7e937abb11b4246215d73
angela stephens <angelastephens317@gmail.com>
2021-03-26 20:33:57 -0400

    update routes resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'sundaes/index'
-  get 'sundaes/create'
-  get 'sundaes/update'
+  resources :sundaes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
