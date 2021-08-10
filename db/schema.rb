9a2a511324053d0706d81749165f38ef8b299fae
angela stephens <angelastephens317@gmail.com>
2021-08-09 21:20:02 -0400

    added to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_16_225811) do
+ActiveRecord::Schema.define(version: 2021_08_03_004653) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2021_03_16_225811) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "ice_cream_flavors"
     t.string "toppings"
+    t.integer "boo_vote"
   end
 
 end
